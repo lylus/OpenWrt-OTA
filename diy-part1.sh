68142dfe276a37bf4bd7e8a738cba9d28f8fea84
Lylus <lylus@qq.com>
2022-10-17 02:33:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+sed -i '$a src-git passwallapp https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
